b5d71992eda566c8b52ae16a5768d44473423b46
jordan gilliam <gilliamja.te@gmail.com>
2021-11-19 11:07:26 -0500

    add button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
 import { styled } from '../stitches.config';
 
-export const Button = styled('button', {
+const Button = styled('button', {
   br: '$pill',
   p: '$2 $3',
-  bg: 'white',
+  bc: 'white',
   fontFamily: '$mono',
   border: 'none',
-  boxShadow:
+  bs:
     '-15px 0 30px -10px var(--colors-orangeA7), 0 0 30px -10px var(--colors-pinkA7), 15px 0 30px -10px var(--colors-violetA7);',
 
   variants: {
@@ -14,14 +14,12 @@ export const Button = styled('button', {
       small: {
         fontSize: '$2',
         height: '$3',
-        paddingRight: '10px',
-        paddingLeft: '10px',
+        px: '$2',
       },
       large: {
-        fontSize: '$5',
+        fontSize: '$3',
         height: '$6',
-        paddingLeft: '15px',
-        paddingRight: '15px',
+        px: '$3',
       },
     },
     color: {
@@ -29,7 +27,7 @@ export const Button = styled('button', {
         backgroundColor: 'white',
         color: 'black',
         '&:hover': {
-          boxShadow:
+          bs:
             '-15px 0 30px -10px var(--colors-orangeA8), 0 0 30px -10px var(--colors-pinkA8), 15px 0 30px -10px var(--colors-violetA8)',
           color: 'black',
         },
@@ -37,3 +35,5 @@ export const Button = styled('button', {
     },
   },
 });
+
+export { Button };
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Button } from '../src/components/Button';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
 
 const meta: Meta = {
   title: 'Button',
@@ -19,14 +20,50 @@ const meta: Meta = {
 
 export default meta;
 
-const Primary: Story = (args) => (
-  <Button color="primary" {...args}>
-    Primary
-  </Button>
+const PrimaryButton: Story = args => (
+  <div style={{ display: 'flex', flexDirection: 'column', gap: 6 }}>
+    <div>
+      <Button color="primary" size="large" {...args}>
+        This is a call to action button{' '}
+        <span>
+          <ArrowRightIcon />
+        </span>
+      </Button>
+    </div>
+  </div>
 );
+
+// const SecondaryButton: Story = args => (
+//   <Button color="secondary" size="small" {...args}>
+//     Secondary
+//   </Button>
+// );
+// const WarnButton: Story = args => (
+//   <div style={{ display: 'flex', flexDirection: 'column', gap: 6 }}>
+//     <div>
+//       <Button color="warn" size="small" {...args}>
+//         Warning!!
+//       </Button>
+//     </div>
+//     <div>
+//       <Button color="error" size="small" {...args}>
+//         Error!!!!
+//       </Button>
+//     </div>
+//     <div>
+//       <Button color="error" icon {...args}>
+//         <PlusIcon />
+//       </Button>
+//     </div>
+//   </div>
+// );
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
-export const Default = Primary.bind({});
+export const Primary = PrimaryButton.bind({});
+// export const Secondary = SecondaryButton.bind({});
+// export const Alert = WarnButton.bind({});
 
-Default.args = {};
+Primary.args = {};
+// Secondary.args = {};
+// Alert.args = {};
